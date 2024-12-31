dc827a33be4bea4dea0ff9bc6fa6d3406c34ceec
MISAMOTZ <siosanmar1234@gmail.com>
2024-12-31 09:53:53 +0700

    Create home.py

---
+++ home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Home")
+st.write("Selamat datang di aplikasi Multi-Page untuk Machine Learning!")
+st.write("Gunakan menu di sidebar untuk berpindah antar halaman.")
